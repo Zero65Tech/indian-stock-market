7d1bc726636c8bc99a2d9925568eec2eb598ede0
Prashant Gupta <antshpra@gmail.com>
2023-11-21 17:58:37 +0530

    Test Suite

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { info, expiry, isHoliday, isOpen, hasOpened, hasClosed } = require('../src/index')
+const { info, isHoliday, isOpen, hasOpened, hasClosed } = require('../src/index')
 
 
 
@@ -10,36 +10,23 @@ const infoTestCases = [
   [ "WIPRO",    { script: "WIPRO"    }],
 
   // FO · FUT
-  ["RELIANCE23NOVFUT", { script: "RELIANCE", expiry: "23NOV", type: "FUT" }],
+  ["RELIANCE23NOVFUT", { script: "RELIANCE", exp: "23NOV", expiry:"2023-11-30", type: "FUT" }],
 
   // FO · CE
-  [ "RELIANCE23APR2300CE",  { script: "RELIANCE",  expiry: "23APR", strike: 2300, type: "CE" } ], // Stock, Monthly Expiry
-  [ "654GS203223NOV91.5CE", { script: "654GS2032", expiry: "23NOV", strike: 91.5, type: "CE" } ], // ??, Monthly Expiry
+  [ "RELIANCE23APR2300CE",  { script: "RELIANCE",  exp: "23APR", expiry:"2023-04-29", strike: 2300, type: "CE" } ], // Stock, Monthly Expiry
+  [ "654GS203223NOV91.5CE", { script: "654GS2032", exp: "23NOV", expiry:"2023-11-30", strike: 91.5, type: "CE" } ], // ??, Monthly Expiry
 
   // FO · PE
-  [ "BANKNIFTY23APR40000PE", { script: "BANKNIFTY", expiry: "23APR", strike: 40000, type: "PE" } ], // Index, Monthly Expiry
-  [ "BANKNIFTY23D0643000PE", { script: "BANKNIFTY", expiry: "23D06", strike: 43000, type: "PE" } ], // Index, ??
-  [ "BANKNIFTY20N0523500PE", { script: "BANKNIFTY", expiry: "20N05", strike: 23500, type: "PE" } ], // Index, ??
-  [ "COALINDIA21JUL142.5PE", { script: "COALINDIA", expiry: "21JUL", strike: 142.5, type: "PE" } ], // Stock, Monthly Expiry
+  [ "BANKNIFTY23APR40000PE", { script: "BANKNIFTY", exp: "23APR", expiry:"2023-04-29", strike: 40000, type: "PE" } ], // Index, Monthly Expiry
+  [ "BANKNIFTY23D0643000PE", { script: "BANKNIFTY", exp: "23D06", expiry:"2023-12-06", strike: 43000, type: "PE" } ], // Index, ??
+  [ "BANKNIFTY20N0523500PE", { script: "BANKNIFTY", exp: "20N05", expiry:"2020-11-05", strike: 23500, type: "PE" } ], // Index, ??
+  [ "COALINDIA21JUL142.5PE", { script: "COALINDIA", exp: "21JUL", expiry:"2021-07-29", strike: 142.5, type: "PE" } ], // Stock, Monthly Expiry
 
 ];
 
 describe(".info(symbol)", () => {
   for (let [symbol, output] of infoTestCases)
     test(symbol, () => expect(info(symbol)).toStrictEqual(output));
-});
-
-
-
-const expiryTestCases = [
-  ["23JUN", "2023-06-28"], // Wed
-  ["23SEP", "2023-09-28"], // Thu
-  ["23NOV", "2023-11-30"], // Thu
-];
-
-describe(".expiry(ddmon)", () => {
-  for (let [ddmon, date] of expiryTestCases)
-    test(ddmon, () => expect(expiry(ddmon)).toBe(date));
 });
 
 
@@ -71,7 +58,7 @@ const dateTimeTestCases = [
 for(const [ dateTime, bool1, bool2, bool3, bool4 ] of dateTimeTestCases) {
   describe(dateTime, () => {
 
-    beforeAll(() => jest.useFakeTimers({ now: new Date(dateTime) }))
+    beforeAll(() => jest.useFakeTimers({ now: new Date(dateTime) }));
 
     test('.isHoliday(dateStr)', () => expect(isHoliday(dateTime.substring(0,10))).toBe(bool1));
 
