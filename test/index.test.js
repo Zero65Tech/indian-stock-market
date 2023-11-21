fd99ecf8234286f69c0eb25810f3379a0ed1c863
Prashant Gupta <antshpra@gmail.com>
2023-11-21 17:48:07 +0530

    Test Suite

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { info, expiry } = require('../src/index')
+const { info, expiry, isHoliday, isOpen, hasOpened, hasClosed } = require('../src/index')
 
 
 
@@ -41,3 +41,45 @@ describe(".expiry(ddmon)", () => {
   for (let [ddmon, date] of expiryTestCases)
     test(ddmon, () => expect(expiry(ddmon)).toBe(date));
 });
+
+
+
+const dateTimeTestCases = [
+
+  // Monday
+  [ '2023-01-02T08:59:59+05:30', false, false, false, false ],
+  [ '2023-01-02T09:00:00+05:30', false, true,  true,  false ],
+  [ '2023-01-02T15:29:59+05:30', false, true,  true,  false ],
+  [ '2023-01-02T15:30:00+05:30', false, false, true,  true  ],
+
+  // Friday
+  [ '2023-01-06T12:00:00+05:30', false, true, true, false ],
+
+  // Saturday
+  [ '2023-01-07T12:00:00+05:30', true, false, false, false ],
+
+  // Sunday
+  [ '2023-01-08T12:00:00+05:30', true, false, false, false ],
+
+  // Repulic Day
+  [ '2023-01-26T12:00:00+05:30', true, false, false, false ],
+
+  // TODO: Muhurat Day
+
+]
+
+for(const [ dateTime, bool1, bool2, bool3, bool4 ] of dateTimeTestCases) {
+  describe(dateTime, () => {
+
+    beforeAll(() => jest.useFakeTimers({ now: new Date(dateTime) }))
+
+    test('.isHoliday(dateStr)', () => expect(isHoliday(dateTime.substring(0,10))).toBe(bool1));
+
+    test('.isHoliday()', () => expect(isHoliday()).toBe(bool1));
+    test('.isOpen()',    () => expect(isOpen())   .toBe(bool2));
+    test('.hasOpened()', () => expect(hasOpened()).toBe(bool3));
+    test('.hasClosed()', () => expect(hasClosed()).toBe(bool4));
+
+  })
+}
+
