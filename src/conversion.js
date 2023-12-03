ac7f4351b907a079ce7bdfad136c8b4a1ea26063
jaigupta04 <jaigupta004@gmail.com>
2023-12-03 18:30:11 +0530

    using date-fns

--- src/conversion.js
+++ src/conversion.js
@@ -1,15 +1,15 @@
 const holidays = require("./holidays.js");
-const moment   = require("moment");
+const { format, parse } = require('date-fns');
 const fs       = require("fs");
 
 const dateObject = {};
 
 holidays.forEach((datearray) => {
   datearray.forEach((datestr) => {
-    const date  = moment(datestr, 'YYYY-MM-DD');
-    const year  = date.year();
-    const month = date.month() + 1;
-    const day   = date.date();
+    const date = parse(datestr, 'yyyy-MM-dd', new Date());
+    const year = format(date, 'yyyy');
+    const month = format(date, 'M');
+    const day = format(date, 'd');
 
     if (!dateObject[year]) dateObject[year] = {};
 
