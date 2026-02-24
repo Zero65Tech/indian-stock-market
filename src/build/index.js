ff9f06b79486edaaf0edfc295d9946a1f4d58979
Anika Gupta <anikagupta0316@gmail.com>
2026-02-24 10:53:26 +0000

    minor refactoring

--- src/build/index.js
+++ src/build/index.js
@@ -5,18 +5,16 @@ import specialDays from "../config/special-days.js";
 
 let minified = {};
 
-for (let dateArr of holidays) {
-  for (let date of dateArr) {
-    let [year, month, day] = date.split("-");
-    year = parseInt(year);
-    month = parseInt(month);
-    day = parseInt(day);
+for (let date of holidays) {
+  let [year, month, day] = date.split("-");
+  year = parseInt(year);
+  month = parseInt(month);
+  day = parseInt(day);
 
-    minified[year] = minified[year] || {};
-    minified[year][month] = minified[year][month] || [];
+  minified[year] = minified[year] || {};
+  minified[year][month] = minified[year][month] || [];
 
-    minified[year][month].push(day);
-  }
+  minified[year][month].push(day);
 }
 
 fs.writeFileSync("src/build/holidays.json", JSON.stringify(minified));
